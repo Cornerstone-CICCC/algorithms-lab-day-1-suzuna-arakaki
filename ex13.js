c6e2d335a34881e1b2331ac457ef3fd9b2f25b03
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-23 21:02:30 -0700

    exercise 13 edited

--- ex13.js
+++ ex13.js
@@ -1,8 +1,16 @@
-// Exercise 13: Write a function named 'findById' that takes an array of objects and an id, and returns the object with the matching id, or null if not found.
+// Exercise 13: Write a function named 'findById' that takes an array of objects and an id, 
+// and returns the object with the matching id, or null if not found.
 
 function findById(arr, id) {
-  // your code here
-  return arr.find(arrId => arrId.id === id) || null;
+  //配列内でid:2と一致するものを探す
+  //見つけたらそのジェクトごと表示
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i].id === id){
+      return arr[i];
+    }
+  }
 }
 
 console.log(findById([{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }], 2)) // { id: 2, name: 'Jane' }
+
+// return arr.find(arrId => arrId.id === id) || null;
