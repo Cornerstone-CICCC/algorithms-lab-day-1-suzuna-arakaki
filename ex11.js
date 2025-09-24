a0961e0074bbb893291db6e5cd1905a8dabcb78d
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-23 20:50:33 -0700

    exercise 11 edited

--- ex11.js
+++ ex11.js
@@ -1,9 +1,6 @@
 // Exercise 11: Write a function named 'reverseWords' that takes a string of text and returns the string with the order of the words reversed.
 
 function reverseWords(text) {
-  //文字列をスペースごとに取り出して配列にす
-  //配列を逆に並べる
-  //配列を繋げて文字列にする、スペースありで
   const result = text.split(' ').reverse().join(" ");
   return result;
 }
