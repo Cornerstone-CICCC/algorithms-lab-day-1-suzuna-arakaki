431543c630f713836e05cf58124dc47ff3f37ad9
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-23 20:55:38 -0700

    exercise 12 edited

--- ex12.js
+++ ex12.js
@@ -1,8 +1,19 @@
-// Exercise 12: Write a function named 'filterAdults' that takes an array of user objects (each with an 'age' property) and returns a new array with only the users who are 18 or older.
+// Exercise 12: Write a function named 'filterAdults' that takes an array of user objects (each with an 'age' property) 
+// and returns a new array with only the users who are 18 or older.
 
 function filterAdults(users) {
-  // your code here
-  return users.filter(user => user.age >= 18);
+  //年齢が18歳以上の人だけを抽出
+  //オブジェクトごと表示
+  const adults = [];
+  for (let i = 0; i < users.length; i++){
+    if (users[i].age >= 18){
+      adults.push(users[i]);
+    }
+  }
+  return adults;
 }
 
-console.log(filterAdults([{ name: 'Jane', age: 18 }, { name: 'Joe', age: 19 }, { name: 'Jim', age: 17 }])) // [{ name: 'Jane', age: 18 }, { name: 'Joe', age: 19 }]
+console.log(filterAdults([{ name: 'Jane', age: 18 }, { name: 'Joe', age: 19 }, { name: 'Jim', age: 17 }])) 
+// [{ name: 'Jane', age: 18 }, { name: 'Joe', age: 19 }]
+
+// return users.filter(user => user.age >= 18);
