5100e394b4891cc4abf22c8b3e8bfe652f828b98
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-24 00:16:33 -0700

    Deleted some files i didn't work

--- ex08.js
+++
@@ -1,7 +0,0 @@
-// Exercise 8: Write a function named 'longestWord' that takes a string of text and returns the longest word in the string.
-
-function longestWord(text) {
-  // your code here
-}
-
-console.log(longestWord('The duck loves bananas and grapes')) // 'bananas'
