364dd04cc74dc36aebbf4b9dd4cdd54305569774
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-23 18:49:51 -0700

    exercise 3 edited

--- ex03.js
+++ ex03.js
@@ -1,8 +1,18 @@
 // Exercise 3: Write a function named 'getFullNames' that takes an array of user objects (each with 'firstName' and 'lastName' properties) and returns an array of full names (e.g., 'John Doe'). 
 
 function getFullNames(users) {
-  // your code here
-  return users.map(user => `${user.firstName} ${user.lastName}`);
+  // 配列ないのオブジェクトを新しい変数に取り出して、firstname lastnameをくっつける
+  let fullName = [];
+  for (let i = 0; i < users.length; i++){
+
+    let result = `${users[i].firstName} ${users[i].lastName}`;
+    result += fullName.push(result);
+  }
+  return fullName;
 }
 
 console.log(getFullNames([{ firstName: 'John', lastName: 'Doe' }, { firstName: 'Jane', lastName: 'Doe' }])) // ['John Doe', 'Jane Doe']
+
+
+
+  // return users.map(user => `${user.firstName} ${user.lastName}`);
